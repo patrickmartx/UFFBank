45678ebb2226a3d92f081bf118b70c897bfe05d2
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-11-10 12:24:00 -0300

    feat: criado controller para página sem login

---
+++ src/java/dev/controller/Controller.java
@@ -0,0 +1,79 @@
+package dev.controller;
+
+import dev.exceptions.NonExistingURLException;
+import dev.model.DAO;
+import dev.model.complements.DAOUser;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Patrick
+ */
+@WebServlet(name = "Controller", urlPatterns = {"/Controller", "/Login", "/Login/Auth",
+                                                "/Home"})
+public class Controller extends HttpServlet {
+
+    DAOUser dao = new DAOUser();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getServletPath();
+
+        switch (action) {
+            case "/Login" -> {
+                dao.testeConexao();
+                RequestDispatcher rd = request.getRequestDispatcher("/views/Login.jsp");
+                rd.forward(request, response);
+            }
+            case "/Home" -> {
+                RequestDispatcher rd = request.getRequestDispatcher("/views/Home.jsp");
+                rd.forward(request, response);
+            }
+            default ->
+                throw new NonExistingURLException("Essa URL não é aceita: " + action);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String action = request.getServletPath();
+
+        switch (action) {
+            case "/Login/Auth" -> {
+                /*
+                Validar conta
+                */
+                String username = (String) request.getParameter("username");
+                String password = (String) request.getParameter("password");
+                System.out.println(username);
+                System.out.println(password);
+
+                response.sendRedirect("/UFFBank/Home");
+            }
+            case "" -> {
+            }
+            default ->
+                throw new NonExistingURLException("Essa URL não é aceita: " + action);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
