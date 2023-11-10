73d97642ce7e1657c35cc99c71db8acc15fdad45
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-11-10 12:32:13 -0300

    feat: criado camada de serviços para Conta no banco

---
+++ src/java/dev/services/BankAccountService.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.services;
+
+/**
+ *
+ * @author Patrick
+ */
+public interface BankAccountService {
+    
+    public void save(Double saldo, Integer agencia, String numConta);
+}
