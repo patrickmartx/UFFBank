250904ff4b8b7a1d4d618d829ddabdecad826943
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-11-29 12:47:41 -0300

    feat: Adcionado serviços de retorno de usuário pelo cpf e senha e o retorno do saldo.

--- src/java/dev/services/impl/ClientServiceImpl.java
+++ src/java/dev/services/impl/ClientServiceImpl.java
@@ -14,12 +14,18 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import dev.exceptions.NoEntityFoundException;
 
 /**
  *
  * @author Patrick
  */
 public class ClientServiceImpl implements ClientService {
+    private ClientRepository repository;
+    
+    public ClientServiceImpl() {
+        this.repository = new ClientRepository();
+    }
 
     @Override
     public Client getById(Long id) {
@@ -44,5 +50,25 @@ public class ClientServiceImpl implements ClientService {
     @Override
     public void deleteById(Long id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    public Client getClientByLogin(String cpf, String password) {
+        try {
+            Client existingClient = new Client();
+            existingClient = repository.getByLogin(cpf, password);
+            if (!existingClient.equals(null)) {
+                return existingClient;
+            }
+            else
+                throw new NoEntityFoundException();
+        } catch(Exception ex) {
+            Logger.getLogger(ClientServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            throw  new RuntimeException("Ocorreu algum erro ao buscar o cliente.");
+        }
+    }
+    
+    public Double getAccountBalance(){
+        Double accountBalance = repository.getAccountBalance();
+        return accountBalance;
     }
 }
